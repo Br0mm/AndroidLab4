4d3387f9a5aee0470b92f06c76a94ddf6d040eaa
Br0mm <Andrew-Ost@mail.ru>
2021-11-17 09:35:54 +0300

    new tests added

--- app/src/androidTest/java/com/example/myapplication/NavigationTest.kt
+++ app/src/androidTest/java/com/example/myapplication/NavigationTest.kt
@@ -1,7 +1,8 @@
 package com.example.myapplication
 
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.NoActivityResumedException
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
@@ -24,160 +25,219 @@ class NavigationTest {
 
     @Test
     fun testAbout() {
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         openAbout()
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
     fun testBackToFragment1From2() {
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test
     fun testBackToFragment2From3() {
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
     @Test(expected = NoActivityResumedException::class)
     fun testBackStack() {
         launchActivity<MainActivity>()
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
     @Test(expected = NoActivityResumedException::class)
     fun testBackStack2() {
         launchActivity<MainActivity>()
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
     @Test(expected = NoActivityResumedException::class)
     fun testBackStack3() {
         launchActivity<MainActivity>()
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
         openAbout()
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
     @Test(expected = NoActivityResumedException::class)
     fun testBackStack4() {
         launchActivity<MainActivity>()
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-        Espresso.onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
+        onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
-    @Test
+    @Test(expected = NoActivityResumedException::class)
     fun testAboutReturnToFragment2() {
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        launchActivity<MainActivity>()
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
         openAbout()
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
-    @Test
+    @Test(expected = NoActivityResumedException::class)
     fun testAboutReturnToFragment3() {
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        launchActivity<MainActivity>()
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         openAbout()
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 
     @Test
     fun testChangeOrientation() {
         val activityScenario = launchActivity<MainActivity>()
 
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         activityScenario.recreate()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         activityScenario.recreate()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment1))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
-        Espresso.onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         activityScenario.recreate()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment3))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
         openAbout()
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         activityRule.scenario.onActivity { activity ->
             activity.recreate()
         }
-        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Espresso.pressBack()
-        Espresso.onView(ViewMatchers.withId(R.id.fragment2))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test(expected = NoActivityResumedException::class)
+    fun navigateUpFromFragment2() {
+        launchActivity<MainActivity>()
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .check(ViewAssertions.doesNotExist())
+        pressBack()
+    }
+
+    @Test
+    fun navigateUpFromFragment3() {
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+    @Test
+    fun navigateUpFromFragmentAbout() {
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        openAbout()
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun backstackWithNavigateUp() {
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        openAbout()
+        onView(ViewMatchers.withId(R.id.activity_about)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment3)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .check(ViewAssertions.doesNotExist())
+    }
+
+    @Test(expected = NoActivityResumedException::class)
+    fun backstackWithNavigateUp2() {
+        launchActivity<MainActivity>()
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .check(ViewAssertions.doesNotExist())
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToFirst)).perform(ViewActions.click())
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .check(ViewAssertions.doesNotExist())
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToThird)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.bnToSecond)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment2)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(ViewMatchers.withContentDescription(R.string.nav_app_bar_navigate_up_description))
+            .perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment1)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        pressBack()
     }
 }
